0bb5b11b8568df088ce43eb345ef3261b8c8aca0
문용호 <ntinamu7543@naver.com>
2022-01-22 23:58:16 +0900

    [Test] DB insert 동작 TC 추가
    PageDatabase에 page 데이터 1개가 정상적으로 insert 되는지 확인

---
+++ app/src/androidTest/java/com/yongho/babybook/DatabaseTest.kt
@@ -0,0 +1,47 @@
+package com.yongho.babybook
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.yongho.babybook.data.Page
+import com.yongho.babybook.data.PageDao
+import com.yongho.babybook.data.PageDatabase
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.Before
+import java.time.LocalDate
+
+@RunWith(AndroidJUnit4::class)
+class DatabaseTest {
+
+    private lateinit var pageDatabase: PageDatabase
+    private lateinit var pageDao: PageDao
+
+    @Before
+    fun setup() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        pageDatabase = Room.inMemoryDatabaseBuilder(context, PageDatabase::class.java).build()
+
+        pageDao = pageDatabase.pageDao()
+    }
+
+    @After
+    fun teardown() {
+        pageDatabase.close()
+    }
+
+    @Test
+    fun insertPage_normal_shouldBeInsertedProperly() = runBlocking {
+        val pageData = Page(LocalDate.now().toString(), "Today's my content")
+
+        pageDao.insert(pageData)
+
+        Assert.assertEquals(pageDao.getAll().first()[0], pageData)
+
+    }
+}

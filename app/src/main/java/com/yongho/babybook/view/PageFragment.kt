3f75a3b0523e7b07e34cb7a909b3edea27ccfa4e
문용호 <ntinamu7543@naver.com>
2022-01-27 15:32:39 +0900

    [Feature] 사진 선택 기능 추가
    육아 일기 페이지에서 사진 선택할 수 있는 버튼 추가

--- app/src/main/java/com/yongho/babybook/view/PageFragment.kt
+++ app/src/main/java/com/yongho/babybook/view/PageFragment.kt
@@ -1,10 +1,14 @@
 package com.yongho.babybook.view
 
+import android.app.Activity.RESULT_CANCELED
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -21,6 +25,18 @@ class PageFragment : Fragment() {
 
     private var _binding: FragmentPageBinding? = null
     private val binding get() = _binding!!
+
+    private val galleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        when(it.resultCode) {
+            RESULT_OK -> {
+                Log.d(TAG, "selected image : ${it.data?.data}")
+            }
+
+            RESULT_CANCELED -> {
+                Log.d(TAG, "image select canceled")
+            }
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_page, container, false)
@@ -43,6 +59,14 @@ class PageFragment : Fragment() {
         Log.d(TAG, "date: $date, content: $content")
         pageViewModel.insert(Page(date, content))
         parentFragmentManager.popBackStack()
+    }
+
+    fun onImageButtonClicked() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "image/*"
+        }
+
+        galleryLauncher.launch(intent)
     }
 
     companion object {
